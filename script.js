3407b0f384c4b3a41ec55bc7a21fb7c06aea8b32
HackOverflow404 <medhansh2005@gmail.com>
2023-03-30 17:19:19 +0530

    added box make, game make, and tried to implement all but bugged

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ table.id = "bigBoiTable";
 body.appendChild(table);
 let turn = false; // False = Red, True = Blue
 let enabledBigBox = "all";
+let wholeGameStatus = "";
 let blue = "#85ADD9";
 let red = "#E77471";
 
 let tixTaxMatrixRender = [];
 let tixTaxMatrixValue = [];
-
+let bigBoardStatus = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
 // The big boi for loop, creates 3 arrays each containing 3 arrays inside of it
 for (let i = 0; i < 3; i++) {
@@ -19,26 +24,26 @@ for (let i = 0; i < 3; i++) {
   table.appendChild(bigRow); // BIG boi roes appended on!!!
   for (let j = 0; j < 3; j++) {
     tixTaxMatrixRender[i][j] = [[], [], []];
-    tixTaxMatrixValue[i][j] = [[], [], []]; 
+    tixTaxMatrixValue[i][j] = [[], [], []];
     const bigBox = document.createElement("table");
     const bigBoxCell = document.createElement("td"); // This is the table data cell to contain this BIG boi boxes
-    bigBoxCell.id = "bigBox" + i + j; 
-    bigBoxCell.className = "bigBox"; 
-    bigBoxCell.appendChild(bigBox); 
-    bigRow.appendChild(bigBoxCell); 
+    bigBoxCell.id = "bigBox" + i + j;
+    bigBoxCell.className = "bigBox";
+    bigBoxCell.appendChild(bigBox);
+    bigRow.appendChild(bigBoxCell);
     for (let k = 0; k < 3; k++) {
-      const smallRow = document.createElement("tr"); 
-      bigBox.appendChild(smallRow); 
-      tixTaxMatrixRender[i][j][k] = [[], [], []]; 
-      tixTaxMatrixValue[i][j][k] = ["", "", ""]; 
+      const smallRow = document.createElement("tr");
+      bigBox.appendChild(smallRow);
+      tixTaxMatrixRender[i][j][k] = [[], [], []];
+      tixTaxMatrixValue[i][j][k] = ["", "", ""];
       for (let l = 0; l < 3; l++) {
         const btn = document.createElement("button");
         btn.id = "box" + i + j + k + l;
         btn.addEventListener("mouseover", buttonOver);
         btn.addEventListener("mouseout", buttonOut);
         btn.addEventListener("click", clickidy);
-        tixTaxMatrixRender[i][j][k][l] = btn; 
-        smallRow.appendChild(document.createElement("td").appendChild(btn)); 
+        tixTaxMatrixRender[i][j][k][l] = btn;
+        smallRow.appendChild(document.createElement("td").appendChild(btn));
       }
     }
   }
@@ -46,7 +51,7 @@ for (let i = 0; i < 3; i++) {
 
 // The function to highlight the button when hovered
 function buttonOver() {
-  if(this.style.backgroundColor != red && this.style.backgroundColor != blue){
+  if (this.style.backgroundColor != red && this.style.backgroundColor != blue) {
     if (!turn) {
       this.style.backgroundColor = red;
     } else {
@@ -57,24 +62,25 @@ function buttonOver() {
 
 // The function to dishighlight? the button when not hovered
 function buttonOut() {
-
   let location = this.id.slice(-4);
-  let value =  tixTaxMatrixValue[location[0]][location[1]][location[2]][location[3]];
+  let value =
+    tixTaxMatrixValue[location[0]][location[1]][location[2]][location[3]];
 
   let color = "#FFFFFF";
-  if( value == "B"){
+  if (value == "B") {
     color = blue;
-  }else if(value == "R"){
+  } else if (value == "R") {
     color = red;
   }
   this.style.backgroundColor = color;
-
-  
 }
 
 // Da function fo wen da user does da ting where they do da clik
 function clickidy() {
-  // console.log("one of 'em did da clickin' on the box called " + this.id);
+  console.log(
+    "%c one of 'em did da clickin' on the box called " + this.id,
+    "color: " + (turn ? blue : red)
+  );
   let location = this.id.slice(-4);
 
   if (turn) {
@@ -89,51 +95,167 @@ function clickidy() {
 
   enabledBigBox = [location[2], location[3]];
 
-  checkIfBoxMade();
+  bigBoardStatus[location[0]][location[1]] = checkIfBoxMade(
+    // Check if small board made
+    tixTaxMatrixValue[location[0]][location[1]]
+  );
+  console.table([
+    location[0],
+    location[1],
+    bigBoardStatus[location[0]][location[1]],
+  ]);
+  wholeGameStatus = checkIfBoxMade(bigBoardStatus);
+  console.log(wholeGameStatus); // Check if whole game ended
+  if (wholeGameStatus != "") {
+    console.log("Game Ended!!!! Won: " + wholeGameStatus); // Check if whole game ended
+    endGame();
+  }
   updateRender(tixTaxMatrixValue);
 }
 
-function checkIfBoxMade() {
-  // here we do the algorithm to check if BIG boi box made
+function endGame() {
+  for (let i = 0; i < tixTaxMatrixRender.length; i++) {
+    for (let j = 0; j < tixTaxMatrixRender[i].length; j++) {
+      for (let k = 0; k < tixTaxMatrixRender[i][j].length; k++) {
+        for (let l = 0; l < tixTaxMatrixRender[i][j][k].length; l++) {
+          tixTaxMatrixRender[i][j][k][l].disabled = true;
+        }
+      }
+    }
+  }
 }
 
+function checkIfBoxMade(board) {
+  let boardStatus = ["D", "R", "B", ""];
+  const winningCombos = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ],
+  ];
 
+  for (let i = 0; i < winningCombos.length; i++) {
+    const combo = winningCombos[i];
+    const [a, b, c] = combo;
 
+    if (
+      board[a[0]][a[1]] == board[b[0]][b[1]] &&
+      board[b[0]][b[1]] == board[c[0]][c[1]]
+    ) {
+      if (board[a[0]][a[1]] == "R") {
+        return boardStatus[1];
+      } else if (board[a[0]][a[1]] == "B") {
+        return boardStatus[2];
+      }
+    }
+  }
 
+  let playedBoxNum = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] != "") {
+        playedBoxNum++;
+      }
+    }
+  }
+  if (playedBoxNum == 9) {
+    return boardStatus[0];
+  }
 
+  return boardStatus[3];
+}
+
+// Matches TixTaxMatrix Render to tixTaxMatrixValue
 function updateRender(matrixValue) {
-  // Matches TixTaxMatrix Render to tixTaxMatrixValue
-
-  for(let i = 0; i < 3; i++){
-    for(let j = 0; j < 3; j++){
-      let miniRender =tixTaxMatrixRender[i][j];
+  // Any board rule
+  if (bigBoardStatus[enabledBigBox[0]][enabledBigBox[1]] != "") {
+    console.log("all");
+    console.log(bigBoardStatus);
+    enabledBigBox = "all";
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let miniRender = tixTaxMatrixRender[i][j];
+        for (let k = 0; k < 3; k++) {
+          for (let l = 0; l < 3; l++) {
+            miniRender[k][l].disabled = true;
+            if (
+              tixTaxMatrixValue[i][j][k][l] == "" &&
+              bigBoardStatus[i][j] == ""
+            ) {
+              miniRender[k][l].disabled = false;
+            }
+          }
+        }
+      }
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let miniRender = tixTaxMatrixRender[i][j];
       let miniValue = matrixValue[i][j];
 
-      if(enabledBigBox[1] == j && enabledBigBox[0] == i){
-        //tixTaxMatrixRender[i][j].backgroundColor == "#FF00FF";
+      if (enabledBigBox[1] == j && enabledBigBox[0] == i) {
         console.log(tixTaxMatrixRender[i][j]);
       }
 
-
       for (let k = 0; k < 3; k++) {
         for (let l = 0; l < 3; l++) {
-
           // Sets Whether Boxes are Disabled/Enabled
           miniRender[k][l].disabled = true;
-          if(enabledBigBox[1] == j && enabledBigBox[0] == i){
+          if (
+            enabledBigBox[0] == i &&
+            enabledBigBox[1] == j &&
+            tixTaxMatrixValue[i][j][k][l] == "" &&
+            bigBoardStatus[i][j] == ""
+          ) {
             miniRender[k][l].disabled = false;
           }
 
-          if(miniValue[k][l] == 'R'){
+          if (miniValue[k][l] == "R") {
             miniRender[k][l].style.backgroundColor = red;
-          } else if(miniValue[k][l] == 'B'){
+          } else if (miniValue[k][l] == "B") {
             miniRender[k][l].style.backgroundColor = blue;
           } else {
             miniRender[k][l].style.backgroundColor = "#FFFFFF";
           }
         }
       }
-
     }
   }
 }
